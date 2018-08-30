bf58034aae9bd830448bf397f1f1574d9d07a945
Munira Omar <munniomer@gmail.com>
2018-08-30 13:12:40 -0700

    first commmit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi Am Munira Omar.</h1>
+    <p>Am a girl who loves coding and I live in Nairobi.</p>
+    <p>I would love to take part the JAMStack online hackathon that freeCodeCamp is hosting on oct27-28.</p>
+    {/*<Link to="/page-2/">Go to page 2</Link>*/}
+  </div>
+)
+
+export default IndexPage
